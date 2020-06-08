0f994b8eaa3b948fa268d052be7c25ebca213904
Avatadahmad <attarad@mail.uc.edu>
2020-06-08 00:14:32 -0400

    Update-2

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const ENV = process.env.NODE_ENV;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 var db = require("./database");
 const app = express();
